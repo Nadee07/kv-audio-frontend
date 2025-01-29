74422cce1899558ced283e12cab4bb8f43947340
Nadee07 <nadeerawithana2k20@gmail.com>
2025-01-30 01:26:25 +0530

    implement admin dashboard...but some tailwind css errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
 
-import './App.css'
-import ProductCart from './components/productCart'
+import "./App.css";
+import ProductCart from "./components/productCart";
+import { MdBluetoothAudio } from "react-icons/md";
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdOutlineSpeaker } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
 
 function App() {
 
   return (
-    <>
-     
-      <ProductCart name = "Audio Setup" price= "3250/=" description ="The **JBL audio mini 3** is a portable Bluetooth speaker with powerful sound, deep bass, and a waterproof design, ideal for outdoor use." img= "https://img.drz.lazcdn.com/static/lk/p/6375915c5fef26e6c88bd489ab15cd4d.jpg_200x200q80.jpg_.webp"/>
-      
-      
-      <ProductCart name = "Sports Headphone" price= "5100/=" description ="SH-S1 Folding Stereo HiFi Sports Headphone Headset with LCD Screen to Display Track Information & SD / TF Card, For Smart Phones & iPad & Laptop & Notebook & MP3 or Other Audio Devices"
-       img= "https://img.drz.lazcdn.com/static/lk/p/eccc900e5a78797610e0549492606f19.jpg_200x200q80.jpg_.webp"/>
-      
-     
+    < div className="w-screen h-screen flex" >
+      <div className="w-[300px] h-screen bg-green-200">
+        <button className="w-full h-[40px] text-black text-[20px] font-bold flex justify-center items-center">
+          <BsGraphDown/>
+          Dashboard
+        </button >
+        <button className="w-full h-[40px] text-black text-[20px] font-bold flex justify-center items-center ">
+          <FaRegBookmark/>
+          Booking
+        </button>
+        <button className="w-full h-[40px] text-black text-[20px] font-bold flex justify-center items-center ">
+          <MdOutlineSpeaker/>
+         Items
+        </button>
+        <button className="w-full h-[40px] text-black text-[20px] font-bold flex justify-center items-center ">
+          <FaRegUser/>
+          Users
+        </button>
 
 
-    </>
-  )
+      </div>
+      <div className="w-full bg-red-600">
+
+
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
