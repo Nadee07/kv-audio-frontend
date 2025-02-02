235592999492e19f4b740612ff4bca657c8b6d88
Nadee07 <nadeerawithana2k20@gmail.com>
2025-02-03 02:28:41 +0530

    Home page routes link error

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Header(){
     return(
-        <header className="w-full h-[100px] shadow-xl flex justify-center">
+        <header className="w-full h-[100px] shadow-xl flex justify-center items-center relative">
+            <img src="/logo.png" alt="logo" className="w-[100px] h-[100px] object-cover border-[3px] absolute left-1 rounded-full "/>
                 <Link to="/" className= "text-[20px] font-bold m-1">Home</Link>
-                <Link to="/contact" className= "text-[20px] font-bold m-1">Contact</Link>
+                <Link to="/Contact" className= "text-[20px] font-bold m-1">Contact</Link>
                     {/*gallery*/}
-                <Link to="/gallery" className= "text-[20px] font-bold m-1">Gallery</Link>
-                <Link to="/items" className= "text-[20px] font-bold m-1">Items</Link>
+                <Link to="/Gallery" className= "text-[20px] font-bold m-1">Gallery</Link>
+                <Link to="/Items" className= "text-[20px] font-bold m-1">Items</Link>
 
 
         </header>
+
     )
 }
--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,9 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
+import ErrorNotFound from "./error";
+import Home from "./home";
+import Items from "./items";
+import Gallery from "./gallery";
+import Contact from "./contact";
 
 export default function HomePage(){
     return(
-        <div className = "w-full h-screen flex justify-center ">
+        <>
             <Header/>
-        </div>
+            <div className="h-[calc(100vh-100px)] w-full"> 
+                 <Routes path="/*">
+                       <Route path= "/Contact" element={<Contact/>}/>
+                       <Route path= "/Gallery" element={<Gallery/>}/>
+                       <Route path= "/Items" element={<Items/>}/>
+                       <Route path= "/" element={<Home/>}/>
+                       <Route path= "/*" element={<ErrorNotFound/>}/>
+
+
+                 
+                 
+                 </Routes>
+
+
+            </div>
+       </>
     )
 }
